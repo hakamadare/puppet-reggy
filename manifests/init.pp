e1622a54d77728b2b950b11ee89184752d78e76f
Sebastian Röder <no-spam-github@sebastianroeder.de>
2013-12-26 20:30:07 +0100

    Update cardboard version to 2.1 in Gemfile

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,7 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Install DTerm.
+class dterm {
+  package { 'DTerm':
+    provider => 'compressed_app',
+    source   => 'http://files.decimus.net/DTerm/DTerm.zip',
+  }
 }
