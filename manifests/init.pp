17b97bb48005f80cd6988ac0eee9830173e58e7e
Steve Huff <shuff@vecna.org>
2014-03-26 14:12:14 -0400

    Leftover commits

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,7 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Install TrackballWorks.
+class trackballworks {
+  package { 'Kensington TrackballWorks':
+    provider => 'pkgdmg',
+    source   => 'http://accoblobstorageus.blob.core.windows.net/software/71f3c09e-e50c-4d71-92be-fd96a9fd13fc.dmg',
+  }
 }
